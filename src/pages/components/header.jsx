491c11a12fe2ffc302f208aeb18a5b9443366b99
GianMarchetti <gian11marchetti@gmail.com>
2024-06-10 18:01:49 -0300

    Header changes

--- src/pages/components/header.jsx
+++ src/pages/components/header.jsx
@@ -1,4 +1,5 @@
 import tableSet from '../../assets/images/tablesetting1.png'
+import fondo from '../../assets/images/fondoset.png'
 import logo from '../../assets/images/logo.png'
 import Image from 'next/image'
 
@@ -8,8 +9,7 @@ const Header = () => {
             {/* <!-- Navbar (sit on top) --> */}
             <nav className="top-0 fixed w-full shadow-md z-50">
                 <ul>
-                    <li><a href="#home" className="font-semibold link">                    <h1 className="w-fit">Traiteurs Chefsito</h1>
-</a></li>
+                    <li><a href="#home" className="font-semibold link"><h1 className="w-fit">Traiteurs Chefsito</h1></a></li>
                     {/* <!-- Right-sided navbar links. Hide them on small screens --> */}
                     <div className="p-3">
                         <li><a href="#about" className="link">About</a></li>
@@ -20,8 +20,11 @@ const Header = () => {
             </nav>
 
             {/* <!-- Header --> */}
-            <header className="w-full min-w-96 relative" id="home">
-                <Image className="banner" src={tableSet} alt="Duck à l'Orange" width="800" height="800" />
+            <header className=" w-full min-w-96 flex flex-row-reverse items-center border-b relative" id="home">
+                <Image className="banner w-full max-h-lvh" src={fondo} alt="Duck à l'Orange" width="800" height="800" />
+                <div className="px-6 py-3 absolute bottom-0 left-0">
+                    <h1 className="text-3xl md:text-6xl">Le Catering Chefsito</h1>
+                </div>
             </header>
         </>
     );
